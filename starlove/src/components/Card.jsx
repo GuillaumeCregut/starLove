4d4a8e135381a2abc9d91e86b5de71761ec52ce7
YacineHATEM <yacine.hatem@gmail.com>
2021-12-21 18:55:54 +0100

    generalecommit

--- starlove/src/components/Card.jsx
+++ starlove/src/components/Card.jsx
@@ -3,13 +3,12 @@ import "./Card.css";
 
 const Card = (props) => {
     return (
-        <div>
-            <div className="size-img">
+
+        <div className="card">
+            <div className="div-img">
                 <img src={props.image} alt={props.name} className="card-img"/>
             </div>
             <p>{props.name}</p>
-
-
         </div>
     )
 }
